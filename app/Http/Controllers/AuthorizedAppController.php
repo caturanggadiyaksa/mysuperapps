e81fd344afe0fd675ee82e4e0c75a79c63d01d82
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-13 17:18:28 +0700

    membuat controler untuk register user api

---
+++ app/Http/Controllers/AuthorizedAppController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\AuthorizedApp;
+
+class AuthorizedAppController extends Controller
+{
+    //
+
+    public function register(Request $request)
+    {
+        // Validasi input dari request
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'redirect_uri' => 'required|url',
+        ]);
+
+        // Generate ID klien dan rahasia klien baru
+        $clientId = bin2hex(random_bytes(16));
+        $clientSecret = bin2hex(random_bytes(32));
+
+        // Simpan informasi aplikasi yang diizinkan ke dalam database
+        $authorizedApp = AuthorizedApp::create([
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'name' => $validatedData['name'],
+            'redirect_uri' => $validatedData['redirect_uri'],
+        ]);
+
+        return response()->json([
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+        ], 201);
+    }
+}
