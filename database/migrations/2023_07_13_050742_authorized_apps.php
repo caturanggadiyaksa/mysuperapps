81d7191a67979a36c0cc9a72a7d9552bfbbb1045
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-13 17:20:17 +0700

    update data ayat alquran pada seeder

---
+++ database/migrations/2023_07_13_050742_authorized_apps.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AuthorizedApps extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('authorized_apps', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('client_id')->unique();
+            $table->string('client_secret');
+            $table->string('name');
+            $table->string('redirect_uri');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('authorized_apps');
+    }
+}
