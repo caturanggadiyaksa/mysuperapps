afee819b363b0ccd56f9e1818f762ad4efc8d63c
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-13 17:26:20 +0700

    add model akses api

---
+++ app/Models/AuthorizedApp.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class AuthorizedApp extends Model
+{
+    // use HasFactory;
+
+    protected $fillable = [
+        'client_id', 'client_secret', 'name', 'redirect_uri',
+    ];
+
+
+}
