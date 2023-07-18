24e323953f7c418d75f2fe06c5a54a16542dae93
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-18 08:07:26 +0700

    add field on migration

--- database/migrations/mysql_blog/2023_07_13_111814_create_detail_user_table.php
+++ database/migrations/mysql_blog/2023_07_13_111814_create_detail_user_table.php
@@ -15,6 +15,15 @@ class CreateDetailUserTable extends Migration
     {
         Schema::create('detail_user', function (Blueprint $table) {
             //
+            $table->id();
+            $table->foreignId('user_id');
+            $table->char('first_name', 30);
+            $table->char('last_name', 30);
+            $table->char('alamat', 200);
+            $table->char('no_telp', 15);
+            $table->enum('jenis_kelamin', ['Laki-laki', 'perempuan']);
+            $table->text('foto_profil');
+            $table->timestamps();
         });
     }
 
