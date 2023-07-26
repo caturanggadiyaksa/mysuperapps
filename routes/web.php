ab555ae083fa3f2e514a3b4524f5a475f59105db
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-26 11:19:30 +0700

    delete all route in web.php

--- routes/web.php
+++ routes/web.php
@@ -8,34 +8,3 @@ use App\Http\Controllers\NavigationController;
 
 
 
-
-
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
-
-
-Route::middleware(['auth', 'admin'])->group(function () {
-    // Routes untuk admin
-
-    Route::get('/admin/dashboard', function () {
-        // Tampilkan halaman admin dashboard
-    });
-});
-
-Route::middleware('auth')->group(function () {
-    // Routes untuk pengguna (user)
-
-    Route::get('/dashboard', function () {
-        // Tampilkan halaman dashboard pengguna
-    });
-
-    Route::get('/profile', [UserController::class, 'profile'])->name('profile');
-    Route::post('/profile', [UserController::class, 'updateProfile'])->name('profile.update');
-});
