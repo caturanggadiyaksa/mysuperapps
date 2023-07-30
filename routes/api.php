8bf60af1b37e62254cc02f464b3a544d80c11862
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-30 19:19:18 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,13 @@ use App\Http\Controllers\QuranController;
 use App\Http\Controllers\AuthorizedAppController;
 use App\Http\Controllers\Blog\BlogController;
 use App\Http\Controllers\Blog\UserBlogController;
-
+use App\Http\Controllers\Blog\DetailUserblogController;
+use App\Http\Controllers\Blog\KategoriBlogController;
+use App\Http\Controllers\Blog\TagBlogController;
+use App\Http\Controllers\Blog\SosmedBlogController;
+use App\Http\Controllers\JuzController;
+use App\Http\Controllers\IndopakController;
+use App\Http\Controllers\UtsmaniController;
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -27,16 +33,28 @@ Route::middleware('auth:sanctum')->group(function () {
 
 
 Route::group(['middleware' => 'verify.api.key'], function () {
+   
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::get('quran', [QuranController::class, 'index']);
     Route::get('quran/{id}', [QuranController::class, 'show']);
     Route::get('quran/{id}/ayat/{ayatId}', [QuranController::class, 'showAyat']);
     Route::post('surahs', [QuranController::class, 'store']);
-    
+    Route::apiResource('/juz', JuzController::class);
+    Route::apiResource('/indopak', IndopakController::class);
+    Route::apiResource('/utsmani', UtsmaniController::class);
+
 });
+
+
 
 
 Route::post('apps/register', [AuthorizedAppController::class, 'register']);
 
 Route::apiResource('/blog', BlogController::class);
 Route::apiResource('/userblog', UserBlogController::class);
+Route::apiResource('/detailuserblog', DetailUserblogController::class);
+Route::apiResource('/tag', TagBlogController::class);
+Route::apiResource('/sosmed', SosmedBlogController::class);
+Route::apiResource('/kategori', KategoriBlogController::class);
+
+
